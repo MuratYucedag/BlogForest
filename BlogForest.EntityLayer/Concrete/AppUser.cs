dd6c0f64415ab8b88d2ff2a50b17ffbb2f237b86
MuratYucedag <yucedagmurat23@gmail.com>
2024-05-21 21:33:48 +0300

    ...

--- BlogForest.EntityLayer/Concrete/AppUser.cs
+++ BlogForest.EntityLayer/Concrete/AppUser.cs
@@ -13,5 +13,6 @@ namespace BlogForest.EntityLayer.Concrete
         public string Surname { get; set; }
         public string ImageUrl { get; set; }
         public string Description { get; set; }
+        public List<Blog> Blogs { get; set; }
     }
 }
