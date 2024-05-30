12561c5552aa8f78518dad56a87ecb294ac5d70f
MuratYucedag <yucedagmurat23@gmail.com>
2024-05-30 20:38:13 +0300

    ....

--- BlogForest.WebUI/Controllers/RoleController.cs
+++ BlogForest.WebUI/Controllers/RoleController.cs
@@ -65,10 +65,35 @@ namespace BlogForest.WebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> UpdateRole(UpdateRoleViewModel updateRoleViewModel)
         {
-            var value=_roleManager.Roles.FirstOrDefault(x=>x.Id == updateRoleViewModel.RoleId);
-            value.Name= updateRoleViewModel.RoleName;
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == updateRoleViewModel.RoleId);
+            value.Name = updateRoleViewModel.RoleName;
             await _roleManager.UpdateAsync(value);
             return RedirectToAction("RoleList");
+        }
+
+        public IActionResult UserList()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleId = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(model);
+            }
+            return View(roleAssignViewModels);
         }
     }
 }
